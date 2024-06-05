9ffa46315520b3b0128141a804cd3cf1f82410ea
thangdx-1076 <dang.xuan.thang@sun-asterisk.com>
2024-06-05 22:41:44 +0700

    feat: update

--- src/app/intersection-observe/components/Todo.tsx
+++ src/app/intersection-observe/components/Todo.tsx
@@ -2,30 +2,31 @@ import { useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
 export default function Todo({ todo }: { todo: any }) {
-  const ref = useRef(null);
-  const [inView, setInView] = useState(false);
-  // const { ref, inView } = useInView({
-  //   threshold: 0.5,
-  // });
+  // const ref = useRef(null);
+  // const [inView, setInView] = useState(false);
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    root: document.querySelector(".scroller-id"),
+  });
 
-  useEffect(() => {
-    const observe = new IntersectionObserver(
-      (entries) => {
-        window.requestAnimationFrame(() => {
-          console.log("entries", entries);
-          for (const entry of entries) {
-            setInView(entry.isIntersecting && entry.intersectionRatio >= 0.5);
-          }
-        });
-      },
-      {
-        root: document.querySelector(".scroller-id"),
-        threshold: 0.5,
-      }
-    );
+  // useEffect(() => {
+  //   const observe = new IntersectionObserver(
+  //     (entries) => {
+  //       window.requestAnimationFrame(() => {
+  //         console.log("entries", entries);
+  //         for (const entry of entries) {
+  //           setInView(entry.isIntersecting && entry.intersectionRatio >= 0.5);
+  //         }
+  //       });
+  //     },
+  //     {
+  //       root: document.querySelector(".scroller-id"),
+  //       threshold: 0.5,
+  //     }
+  //   );
 
-    observe?.observe(ref.current);
-  }, []);
+  //   observe?.observe(ref.current);
+  // }, []);
 
   return (
     <div
